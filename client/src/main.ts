d0b22c5c726ee00c7faa425826fb55533b848b1e
averrois <yassine.aitrahou0@gmail.com>
2024-08-17 14:54:54 +0100

    cleanup: update format

--- client/src/main.ts
+++ client/src/main.ts
@@ -1,12 +1,12 @@
-import './assets/main.css'
+import "./assets/main.css"
 
-import { createApp } from 'vue'
+import { createApp } from "vue"
 // @ts-ignore
-import App from './App.vue'
-import router from './router'
+import App from "./App.vue"
+import router from "./router"
 
 const app = createApp(App)
 
 app.use(router)
 
-app.mount('#app')
+app.mount("#app")
