4e1d2e73999dcea660bdcc26f92e5888fc324ba7
averrois <yassine.aitrahou0@gmail.com>
2024-09-04 21:47:59 +0100

    feat: Add Service Model

---
+++ server/src/models/Service.ts
@@ -0,0 +1,23 @@
+import mongoose from "mongoose"
+import { randomUUID } from "crypto"
+
+interface IService extends mongoose.Document {
+  id: string
+  title: string
+  content: string
+}
+
+export const serviceSchema: mongoose.Schema<IService> = new mongoose.Schema({
+  id: {
+    type: String,
+    default: randomUUID(),
+  },
+  title: {
+    type: String,
+    required: true,
+  },
+  content: {
+    type: String,
+    required: true,
+  },
+})
