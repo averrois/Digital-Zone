e469526eb068f4f982a5c7792c3c6de874cee104
averrois <yassine.aitrahou0@gmail.com>
2024-09-07 00:28:28 +0100

    feat: add ServiceView view

---
+++ client/admin/src/views/ServiceView.vue
@@ -0,0 +1,9 @@
+<script setup lang="ts">
+import ServiceCard from "../components/ServiceCard.vue"
+</script>
+
+<template>
+    <div class="w-full grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-6 p-4">
+        <ServiceCard />
+    </div>
+</template>
