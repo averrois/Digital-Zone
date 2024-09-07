885e5b80970ab54d0a7e46b6d0678b8fa8ebc82a
averrois <yassine.aitrahou0@gmail.com>
2024-09-07 15:11:52 +0100

    dpes: install cors types

--- server/src/app.ts
+++ server/src/app.ts
@@ -1,25 +1,20 @@
-import express from 'express';
-// import cors from "cors";
-import appRouter from "./routes";
+import express from "express"
+import cors from "cors"
+import appRouter from "./routes"
 // import cookieParser from "cookie-parser";
 
-const app = express();
+const app = express()
 
 // Enable CORS
-// app.use(
-//   cors({
-//     origin: "http://localhost:5173",
-//     credentials: true,
-//   }),
-// );
+app.use(cors())
 
 // Midedlewares
 // app.use(express.json());
 // process.env.COOKIE_SECRET ? app.use(cookieParser(process.env.COOKIE_SECRET)) : console.log("Missing cookie secret key");
 
 // // Endpoints
-app.use("/api/v1", appRouter);
-console.log("Mounted appRouter successfully");
+app.use("/api/v1", appRouter)
+console.log("Mounted appRouter successfully")
 
 // Export app
-export default app;
+export default app
